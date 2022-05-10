461b643333669ba2cc813554b97360fa34543808
Giordon Stark <kratsg@gmail.com>
2022-05-10 08:20:00 +0200

    update entrypoint to force username and password

--- eos-krb5-shell2http/entrypoint.sh
+++ eos-krb5-shell2http/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+if [ -z "${CERN_USER}" ] || [ -z "${CERN_PASS}" ]; then
+  printf "Must set both CERN_USER and CERN_PASS environment variables.\n"
+  exit 1
+fi
+
+declare -p | grep -E '^CERN_' > /app/container.env || true
+
 set -- /app/shell2http -host="0.0.0.0" "$@"
 
 echo "$@"
