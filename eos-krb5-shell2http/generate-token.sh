df908d9e0d727ac179e8960a658594ca033df350
Giordon Stark <kratsg@gmail.com>
2022-05-10 09:53:21 +0200

    make sure we protect against invalid kerberos tokens

--- eos-krb5-shell2http/generate-token.sh
+++ eos-krb5-shell2http/generate-token.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+[ -f /app/container.env ] && source /app/container.env
+
 STATUS=200
 MESSAGE="Ok."
 
@@ -8,11 +10,21 @@ function print_header {
 }
 
 if [ ! ${v_path+x} ]; then
-  STATUS=503
+  STATUS=400
   MESSAGE="You did not set a path."
   print_header
   printf "{\"message\": \"${MESSAGE}\"}"
   exit 0
 fi
 
-printf "{\"path\": \"${v_path}\", \"message\": \"${MESSAGE}\"}"
+if ! klist 2> /dev/null | grep -q "principal: ${CERN_USER}@CERN.CH"
+then
+  STATUS=503
+  MESSAGE="kerberos is not ready for ${CERN_USER}."
+  print_header
+  printf "{\"message\": \"${MESSAGE}\"}"
+  exit 0
+fi
+
+TOKEN=$(eos token --path "${v_path}")
+printf "{\"token\": \"${TOKEN}\", \"path\": \"${v_path}\", \"message\": \"${MESSAGE}\"}"
