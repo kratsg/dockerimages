5787b79174e7699d0c8418fe9ce79fa5fcaa6b1f
Giordon Stark <kratsg@gmail.com>
2022-06-21 16:43:30 -0400

    add a way to list content

---
+++ eos-krb5-shell2http/ls.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+[ -f /app/container.env ] && source /app/container.env
+
+STATUS=200
+MESSAGE="Ok."
+
+function print_header {
+  printf "Content-Type: application/json\n"
+  printf "Status: ${STATUS}\n\n"
+}
+
+if [ ! ${v_path+x} ]; then
+  STATUS=400
+  MESSAGE="You did not set a path."
+  print_header
+  printf "{\"message\": \"${MESSAGE}\"}"
+  exit 0
+fi
+
+if ! klist 2> /dev/null | grep -q "principal: ${CERN_USER}@CERN.CH"
+then
+  STATUS=503
+  MESSAGE="kerberos is not ready for ${CERN_USER}."
+  print_header
+  printf "{\"message\": \"${MESSAGE}\"}"
+  exit 0
+fi
+
+CONTENT="$(eos ls "${v_path}" | jq --raw-input . | jq -c --slurp .)"
+print_header
+printf "{\"content\": ${CONTENT}, \"path\": \"${v_path}\", \"message\": \"${MESSAGE}\"}"
