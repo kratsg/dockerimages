23f90d6a56077b40125b05c43f16ad5ce4edb1d0
Giordon Stark <kratsg@gmail.com>
2023-07-20 14:16:36 -0700

    some more changes

--- cern-phonebook/lookup.sh
+++ cern-phonebook/lookup.sh
@@ -27,4 +27,4 @@ else
 fi
 
 print_header
-printf "{\"content\": ${CONTENT}, \"path\": \"${v_path}\", \"message\": \"${MESSAGE}\"}"
+printf "{\"content\": ${CONTENT}, \"pattern\": \"${v_pattern}\", \"message\": \"${MESSAGE}\", \"output\": ${OUTPUT}}"
