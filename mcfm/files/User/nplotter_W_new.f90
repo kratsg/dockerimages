e47fff44fd0458afa0c6e922eef09df3ded602e8
Giordon Stark <kratsg@gmail.com>
2024-02-26 16:12:07 -0800

    missing njets

--- mcfm/files/User/nplotter_W_new.f90
+++ mcfm/files/User/nplotter_W_new.f90
@@ -118,7 +118,7 @@ module nplotter_W
           real(dp) :: phistar, phiacop, costhetastar, delphi34
 
           integer :: countjet, countlept, countneutrino, jetindex(mxpart), leptindex(mxpart),neutrinoindex(mxpart)
-          integer :: j,ijet,ijetmindeltar
+          integer :: j,njets,ijet,ijetmindeltar
           real(dp) :: pjk(4), wcandidate(4)
 
           pt34 = pttwo(3,4,p)
