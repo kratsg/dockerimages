804794a1f83d2d1dba1c6a2e19af81718560be4b
Giordon Stark <kratsg@gmail.com>
2024-03-11 11:51:05 -0700

    update job.sh

--- mcfm/files/Bin/job.sh
+++ mcfm/files/Bin/job.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/sh
 
+# NB: cpus-per-task = 2*floor(128/ntasks-per-node)
+
 #SBATCH --account=m2616
-#SBATCH --qos=regular
+#SBATCH --qos=debug
 #SBATCH --constraint=cpu
 #SBATCH --job-name=mcfm-collinearw
-#SBATCH --nodes=4
-#SBATCH --ntasks=6
-#SBATCH --cpus-per-task=32
+#SBATCH --nodes=6
+#SBATCH --ntasks-per-node=16
+#SBATCH --cpus-per-task=16
 #SBATCH --mail-user=gistark@ucsc.edu
 #SBATCH --mail-type=ALL
 #SBATCH -t 00:30:00
@@ -17,10 +19,10 @@ export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
 # see: https://stackoverflow.com/a/53759961
-srun --mpi=cray_shasta --nodes=1 --ntasks=1 --cpu_bind=cores --job-name='collinear' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=11 -general%runstring=Wp_collinear &
-srun --mpi=cray_shasta --nodes=1 --ntasks=1 --cpu_bind=cores --job-name='collinear' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=16 -general%runstring=Wm_collinear &
-srun --mpi=cray_shasta --nodes=1 --ntasks=1 --cpu_bind=cores --job-name='inclusive' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=11 -general%runstring=Wp_inclusive &
-srun --mpi=cray_shasta --nodes=1 --ntasks=1 --cpu_bind=cores --job-name='inclusive' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=16 -general%runstring=Wm_inclusive &
-srun --mpi=cray_shasta --nodes=1 --ntasks=1 --cpu_bind=cores --job-name='inclusi2j' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=11 -general%runstring=Wp_inclusive2j &
-srun --mpi=cray_shasta --nodes=1 --ntasks=1 --cpu_bind=cores --job-name='inclusi2j' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=16 -general%runstring=Wm_inclusive2j &
+srun --mpi=cray_shasta --nodes=1 --cpu_bind=cores --job-name='collinear' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=11 -general%runstring=Wp_collinear &
+srun --mpi=cray_shasta --nodes=1 --cpu_bind=cores --job-name='collinear' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=16 -general%runstring=Wm_collinear &
+srun --mpi=cray_shasta --nodes=1 --cpu_bind=cores --job-name='inclusive' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=11 -general%runstring=Wp_inclusive &
+srun --mpi=cray_shasta --nodes=1 --cpu_bind=cores --job-name='inclusive' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=16 -general%runstring=Wm_inclusive &
+srun --mpi=cray_shasta --nodes=1 --cpu_bind=cores --job-name='inclusi2j' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=11 -general%runstring=Wp_inclusive2j &
+srun --mpi=cray_shasta --nodes=1 --cpu_bind=cores --job-name='inclusi2j' ./mcfm CollinearW.ini -general%part=snlo -general%nproc=16 -general%runstring=Wm_inclusive2j &
 wait
