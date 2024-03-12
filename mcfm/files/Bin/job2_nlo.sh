f53b755cf73d7f3638280b8480ac7d2ecc5a3ec4
Giordon Stark <kratsg@gmail.com>
2024-03-12 10:42:32 -0700

    add various shell scripts

---
+++ mcfm/files/Bin/job2_nlo.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+
+# NB: cpus-per-task = 2*floor(128/ntasks-per-node)
+
+#SBATCH --account=m2616
+#SBATCH --qos=regular
+#SBATCH --constraint=cpu
+#SBATCH --job-name=mcfm-collinearw
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=256
+#SBATCH --mail-user=gistark@ucsc.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:30:00
+
+export OMP_STACKSIZE=512000
+export OMP_NUM_THREADS=256
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+# see: https://stackoverflow.com/a/53759961
+srun --mpi=cray_shasta --cpu_bind=cores --job-name='collinear W-' ./mcfm CollinearW.ini -general%part=nlo -general%rundir=collinearW_nlo -general%nproc=16 -general%runstring=Wm_collinear
