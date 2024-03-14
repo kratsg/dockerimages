396afed552b41a3c382edabc485ce0ddf7adb59a
Giordon Stark <kratsg@gmail.com>
2024-03-14 05:21:34 -0700

    reduce integration goal a bit more

--- mcfm/files/Bin/job5_snlo.sh
+++ mcfm/files/Bin/job5_snlo.sh
@@ -19,4 +19,4 @@ export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 
 # see: https://stackoverflow.com/a/53759961
-srun --mpi=cray_shasta --cpu_bind=cores --job-name='inclusi2j W+' ./mcfm CollinearW.ini -general%part=snlo -general%rundir=collinearW_snlo_v3 -general%nproc=11 -general%runstring=Wp_inclusive2j -nnlo%taucut=0.01
+srun --mpi=cray_shasta --cpu_bind=cores --job-name='inclusi2j W+' ./mcfm CollinearW.ini -general%part=snlo -general%rundir=collinearW_snlo_v3 -general%nproc=11 -general%runstring=Wp_inclusive2j -nnlo%taucut=0.01 -integration%precisiongoal=0.0001
